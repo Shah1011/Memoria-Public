84143a97ea02231bbe81801e4a99cbc58d703f52
DanishQ1011 <officiallyshah@outlook.com>
2024-07-18 20:34:22 +0530

    Update firebase.ts

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -9,12 +9,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCCaV8QQqJrQMC7a1nvdzQNlD0b2Gs8VRg",
-  authDomain: "memoria-fbc42.firebaseapp.com",
-  projectId: "memoria-fbc42",
-  storageBucket: "memoria-fbc42.appspot.com",
-  messagingSenderId: "810756943856",
-  appId: "1:810756943856:web:7fa3e6ff6e45f66a68f735"
+  apiKey: process.env.FIREBASE_,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
